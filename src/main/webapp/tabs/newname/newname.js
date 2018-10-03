a680f5dc42273ade1e60cda672838d5fb3c14537
jeffrpowell <jeffr.powell@outlook.com>
2018-10-03 06:09:23 -0600

    trying to prep names for submission

--- src/main/webapp/tabs/newname/newname.js
+++ src/main/webapp/tabs/newname/newname.js
@@ -7,6 +7,48 @@ $(document).ready(function() {
 	});
 	
 	$("#addNames").click(function() {
-		$("#newNames").append($newNameTemplate);
+        var count = parseInt($("#addNewNames [name='new-name-count']").val());
+		for (; count > 0; count--) {
+            $("#newNames").append($newNameTemplate);
+        }
 	});
+    
+    $("#submitNames").click(function() {
+        var submissions = $('#newNames .new-name').each(function(i, nameField){
+//            submission = {   
+//                familySearchId: "123-4567",
+//                pdf:[],
+//                ordinances:[],
+//                supplier:{
+//                    id: "1",
+//                    email: "email@email.com",
+//                    phone: "123-123-4567",
+//                    name: "name"
+//                }
+//            };
+            return {
+                familySearchId: $(this).find("[name='name-id']").val(),
+                pdf: $(this).find("[name='name-pdf']").val(),
+                ordinances: $(this).find("[name='name-ordinances']:checked").val(),
+                submitter: getWardMemberObject()
+            };
+        });
+        console.log(submissions);
+    });
+    
+    function getWardMemberObject() {
+//            {
+//                id: "1",
+//                name: "name",
+//                email: "email@email.com",
+//                phone: "123-123-4567"
+//            }
+        return {
+            id: $("#wardMember [name='ward-member-id']").val(),
+            name: $("#wardMember [name='ward-member-name']").val(),
+            email: $("#wardMember [name='ward-member-email']").val(),
+            phone: $("#wardMember [name='ward-member-phone']").val()
+        };
+    }
+    
 });
